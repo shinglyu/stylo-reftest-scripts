1113bb894abc123bb9e72933ab107ee10e3619d8
Shing Lyu <slyu@mozilla.com>
2017-01-23 16:40:25 +0800

    Updated reftest count script with json output

--- summarize_from_list.sh
+++ summarize_from_list.sh
@@ -7,3 +7,10 @@ echo -e "expected-fails\t$FAIL"
 echo -e "disabled\t$DISABLE"
 echo -en "total\t\t"
 echo $(expr $PASS + $FAIL + $DISABLE)
+
+echo -e "{"
+echo -e "\"date\":$(date +%m\/%d),"
+echo -e "\"reftest_passes\":$PASS,"
+echo -e "\"reftest_fails\":$FAIL,"
+echo -e "\"reftest_disabled\":$DISABLE"
+echo -e "}"
